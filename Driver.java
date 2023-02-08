cd260672a34a83a5e7ad6562d710275ba390b28a
BrittsTest <brittanytest2301@gmail.com>
2023-02-08 21:57:29 +0000

    Start Exercise

---
+++ Driver.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Driver {
+
+    //Cool stuffs to know!!!
+
+    /* 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+	
+	public static void main(String[] args) {
+		NumberSetService numServe = new NumberSetService();
+		DictionaryService dictServe = new DictionaryService();
+		Scanner sc = new Scanner(System.in);
+		// numServe.setMenu(sc);
+		// dictServe.mapMenu(sc);
+	}
+
+}
